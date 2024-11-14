ec0d6852f1b153c92767ea406ef389b9d902760b
Karla Magueta <k.magueta@gmail.com>
2024-11-14 11:00:45 +0000

    implemetação de Smart Look

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -223,3 +223,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
   });
 });
+
+
+// Smart Look
+
+window.smartlook||(function(d) {
+  var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+  var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+  c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+  })(document);
+  smartlook('init', 'ad3875c906935bc2ae62ed1486287db14e777076', { region: 'eu' });
